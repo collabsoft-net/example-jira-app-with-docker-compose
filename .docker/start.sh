60b7c5a004e4d1d5f0fa01a6972d35bfbff0fffb
Remie Bolte <remie@collabsoft.net>
2024-02-01 16:49:52 +0100

    feat: upgrade QuickReload to 5.x and include Jira Data Generator

--- .docker/start.sh
+++ .docker/start.sh
@@ -3,7 +3,8 @@
 mkdir -p /var/atlassian/application-data/jira/plugins/installed-plugins
 cp /opt/dbconfig.xml /var/atlassian/application-data/jira/dbconfig.xml
 cp /opt/quickreload.properties /var/atlassian/application-data/jira/quickreload.properties
-cp /opt/quickreload-3.0.0.jar /var/atlassian/application-data/jira/plugins/installed-plugins/quickreload-3.0.0.jar
+cp /opt/quickreload-5.0.2.jar /var/atlassian/application-data/jira/plugins/installed-plugins/quickreload-5.0.2.jar
+cp /opt/jira-data-generator-5.0.0.jar /var/atlassian/application-data/jira/plugins/installed-plugins/jira-data-generator-5.0.0.jar
 chown -R jira:jira /var/atlassian/application-data/jira
 
 /entrypoint.py
